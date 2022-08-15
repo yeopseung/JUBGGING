55d6a7715544cc2267a101f398185b7e5c7b6cf4
yeopseung <kwak991021@gmail.com>
2022-08-15 14:56:20 +0900

    Main 화면 기본 틀

--- app/src/main/java/org/techtown/my_jubgging/MainActivity.java
+++ app/src/main/java/org/techtown/my_jubgging/MainActivity.java
@@ -1,10 +1,13 @@
 package org.techtown.my_jubgging;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.ranking:
+
+                    case R.id.together:
+
+                    case R.id.home:
+
+                    case R.id.trash_map:
+
+                    case R.id.point_shop:
+
+                }
+            }
+        });
     }
 }
