b9736b7538b114ea5dbd71cd6e4de48bcb66c8ab
yeopseung <kwak991021@gmail.com>
2022-08-17 15:54:21 +0900

    [곽승엽] main 화면 BottomNavigationView, Fragment 화면 구성

---
+++ app/src/main/java/org/techtown/my_jubgging/fragment/RankingFragment.java
@@ -0,0 +1,23 @@
+package org.techtown.my_jubgging.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.techtown.my_jubgging.R;
+
+
+public class RankingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_ranking, container, false);
+
+        return rootView;
+    }
+}
