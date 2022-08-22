1aa818c52c99bef9f51f7e80b2864e4969a1a817
Arkady <akrady1356@naver.com>
2022-08-23 00:58:12 +0900

    [장재우] Post 충돌 제거

--- app/src/main/java/org/techtown/my_jubgging/Post.java
+++ app/src/main/java/org/techtown/my_jubgging/Post.java
@@ -3,16 +3,6 @@ package org.techtown.my_jubgging;
 import com.google.gson.annotations.SerializedName;
 
 public class Post {
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-    @SerializedName("id")
-    int id;
-    @SerializedName("text")
-    String text;
-    @SerializedName("title")
-    String title;
->>>>>>> master
     @SerializedName("userId")
     String userId = "NULL";
 
@@ -57,79 +47,18 @@ public class Post {
         return region1;
     }
 
-<<<<<<< HEAD
-=======
-    public int getUserId() {
-        return userId;
-=======
-    @SerializedName("userId")
-    String userId = "NULL";
-
-    @SerializedName("region1")
-    String region1 = "";
-
-    @SerializedName("region2")
-    String region2 = "";
-
-    @SerializedName("region3")
-    String region3 = "";
-
-    @SerializedName("title")
-    String title = "Title";
-
-    @SerializedName("content")
-    String content = "Content";
-
-    @SerializedName("peopleNum")
-    int    peopleNum = 3;
-
-    @SerializedName("possibleGender")
-    String possibleGender = "All";
-
-    @SerializedName("localDate")
-    String localDate = "YYYY MM DD";
-
-    @SerializedName("localTime")
-    String localTime = "HH MM";
-
-    @SerializedName("kakaoChatAddress")
-    String kakaoChatAddress = "Address";
-
-    @SerializedName("place")
-    String place = "Place";
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getRegion1() {
-        return region1;
-    }
-
->>>>>>> master
     public String getRegion2() {
         return region2;
     }
 
     public String getRegion3() {
         return region3;
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> master
     }
 
     public String getTitle() {
         return title;
     }
 
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-    public String getText() {
-        return text;
-=======
->>>>>>> master
     public String getContent() {
         return content;
     }
@@ -156,9 +85,5 @@ public class Post {
 
     public String getPlace() {
         return place;
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> master
     }
 }
